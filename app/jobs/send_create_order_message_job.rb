704bdbd800bc06dfb8847c914d879b89203329f7
Theo <bbtfrr@gmail.com>
2016-01-23 15:34:32 +0800

    hotfix response NoMethodError in send_create_order_message_job.rb

--- app/jobs/send_create_order_message_job.rb
+++ app/jobs/send_create_order_message_job.rb
@@ -12,8 +12,7 @@ class SendCreateOrderMessageJob < ActiveJob::Base
     Rails.logger.info "Send order##{order.id} template message to #{users.size} users, skill = #{order.skill}, city = #{order.city}"
     users.each do |user|
       # Rails.logger.info "Send order##{order.id} template message to user##{user.id}"
-      response = Weixin.send_create_order_message(user, order)
-      if response && response.code == 0
+      if Weixin.send_create_order_message(user, order)
         order.mechanic_sent_count += 1
         order.save
       end
